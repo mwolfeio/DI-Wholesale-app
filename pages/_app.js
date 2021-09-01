9648d0929029f2d804e1b2999acf9f830894c0a9
mwoof <matthew.wolfe5@gmail.com>
2021-09-01 16:22:50 -0500

    Revert "testing gql"
    This reverts commit 053f2088bd52083e8945b5969561f9e04c233f4b.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/client";
+import { ApolloProvider } from "react-apollo";
 import App, { Container } from "next/app";
 import Cookies from "js-cookie";
 import { AppProvider } from "@shopify/polaris";
@@ -12,7 +12,7 @@ import ClientRouter from "../components/ClientRouter";
 import "../style/global.css";
 
 const client = new ApolloClient({
-  uri: `https://di-wholesale.myshopify.com/api/shopify/admin/2021-07/graphql`,
+  uri: `${process.env.HOST}/api/shopify/admin/2021-07/graphql`,
   fetch: fetch,
   fetchOptions: {
     credentials: "include",
