7352155092f2671a5f0bad08af7773df73c20df6
mwoof <matthew.wolfe5@gmail.com>
2021-09-02 13:35:59 -0500

    6

--- pages/customers/[id].js
+++ pages/customers/[id].js
@@ -6,15 +6,34 @@ import { gql } from "apollo-boost";
 import Link from "next/link";
 import ButtonNav from "../../components/ButtonNav.js";
 
+const GET_CUSTOMER = gql`
+  {
+    customer(id: "gid://shopify/Customer/1310260789270") {
+      id
+    }
+  }
+`;
+
 const CustomerPage = () => {
   const { id } = useRouter().query;
+  // const { loading, error, data } = useQuery(GET_CUSTOMER);
 
-  let page = "test";
+  // let page = loading ? (
+  //   <div
+  //     style={{ height: "100%", width: "100%" }}
+  //     className="flex-center-center"
+  //   >
+  //     <Loader />
+  //   </div>
+  // ) : error ? (
+  //   `Error! ${error.message}`
+  // ) : (
+  //   <div style={{ width: "100%" }}>test</div>
+  // );
 
   return (
     <main>
       <ButtonNav back="customers" />
-      {page}
     </main>
   );
 };
