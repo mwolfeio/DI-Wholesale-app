1a2f4421eb4a3dd58fef98b4abedf1054f95b344
mwoof <90003090+mwolfeio@users.noreply.github.com>
2021-09-02 14:41:18 -0500

    This is a test of the git permissions

--- pages/customers/[id].js
+++ pages/customers/[id].js
@@ -3,18 +3,18 @@ import { useRouter } from "next/router";
 import { useQuery } from "react-apollo";
 import { gql } from "apollo-boost";
 
-const GET_CUSTOMER = gql`
-  {
-    customer(id: "gid://shopify/Customer/1310260789270") {
-      firstName
-    }
-  }
-`;
+// const GET_CUSTOMER = gql`
+//   {
+//     customer(id: "gid://shopify/Customer/1310260789270") {
+//       firstName
+//     }
+//   }
+// `;
 
 const CustomerPage = () => {
   const { id } = useRouter().query;
-  const { loading, error, data } = useQuery(GET_CUSTOMER);
+  // const { loading, error, data } = useQuery(GET_CUSTOMER);
 
-  return <main>test</main>;
+  return <main>test 900</main>;
 };
 export default CustomerPage;
