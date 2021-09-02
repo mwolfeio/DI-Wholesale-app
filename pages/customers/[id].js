34f5a6471599e4194b8969d8db722885629e928b
mwoof <90003090+mwolfeio@users.noreply.github.com>
2021-09-02 17:01:59 -0500

    testing gql again

--- pages/customers/[id].js
+++ pages/customers/[id].js
@@ -3,17 +3,17 @@ import { useRouter } from "next/router";
 import { useQuery } from "react-apollo";
 import { gql } from "apollo-boost";
 
-// const GET_CUSTOMER = gql`
-//   {
-//     customer(id: "gid://shopify/Customer/1310260789270") {
-//       firstName
-//     }
-//   }
-// `;
+const GET_CUSTOMER = gql`
+  query MyQuery {
+    customer(id: "gid://shopify/Customer/1310260789270") {
+      acceptsMarketing
+    }
+  }
+`;
 
 const CustomerPage = () => {
   const { id } = useRouter().query;
-  // const { loading, error, data } = useQuery(GET_CUSTOMER);
+  const { loading, error, data } = useQuery(GET_CUSTOMER);
 
   return <main>test 900</main>;
 };
