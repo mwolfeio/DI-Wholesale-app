aba2517e64f11267772e9996768023f704e9a178
mwoof <matthew.wolfe5@gmail.com>
2021-09-02 13:34:16 -0500

    5

--- pages/customers/[id].js
+++ pages/customers/[id].js
@@ -6,30 +6,10 @@ import { gql } from "apollo-boost";
 import Link from "next/link";
 import ButtonNav from "../../components/ButtonNav.js";
 
-const GET_CUSTOMER = gql`
-
-    customer(id: "gid://shopify/Customer/1310260789270") {
-      id
-    }
-
-`;
-
 const CustomerPage = () => {
   const { id } = useRouter().query;
-  const { loading, error, data } = useQuery(GET_CUSTOMER);
 
-  let page = loading ? (
-    <div
-      style={{ height: "100%", width: "100%" }}
-      className="flex-center-center"
-    >
-      <Loader />
-    </div>
-  ) : error ? (
-    `Error! ${error.message}`
-  ) : (
-    <div style={{ width: "100%" }}>test</div>
-  );
+  let page = "test";
 
   return (
     <main>
