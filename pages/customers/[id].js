4e21d0ea495e464ce503b18f3a8966711137265e
mwoof <90003090+mwolfeio@users.noreply.github.com>
2021-09-02 17:10:48 -0500

    testing gql query

--- pages/customers/[id].js
+++ pages/customers/[id].js
@@ -4,16 +4,25 @@ import { useQuery } from "react-apollo";
 import { gql } from "apollo-boost";
 
 const GET_CUSTOMER = gql`
-  query MyQuery {
-    customer(id: "gid://shopify/Customer/1310260789270") {
+  query getCustomer($Id: ID!) {
+    customer(id: $Id) {
       acceptsMarketing
     }
   }
 `;
 
+//customer(id: "gid://shopify/Customer/1310260789270")
+
 const CustomerPage = () => {
   const { id } = useRouter().query;
-  const { loading, error, data } = useQuery(GET_CUSTOMER);
+  const { loading, error, data } = useQuery(GET_CUSTOMER, {
+    variables: { Id: `gid://shopify/Customer/${id}` },
+  });
+
+  if (loading) return;
+  if (error) return <p>{error.message}</p>;
+
+  console.log(data ? data : "no data");
 
   return <main>test 900</main>;
 };
