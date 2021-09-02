de8d0fba1a03e5451d9ebdf361d39bd0155c3d68
mwoof <matthew.wolfe5@gmail.com>
2021-09-02 13:37:24 -0500

    7

--- pages/customers/[id].js
+++ pages/customers/[id].js
@@ -16,24 +16,25 @@ const GET_CUSTOMER = gql`
 
 const CustomerPage = () => {
   const { id } = useRouter().query;
-  // const { loading, error, data } = useQuery(GET_CUSTOMER);
+  const { loading, error, data } = useQuery(GET_CUSTOMER);
 
-  // let page = loading ? (
-  //   <div
-  //     style={{ height: "100%", width: "100%" }}
-  //     className="flex-center-center"
-  //   >
-  //     <Loader />
-  //   </div>
-  // ) : error ? (
-  //   `Error! ${error.message}`
-  // ) : (
-  //   <div style={{ width: "100%" }}>test</div>
-  // );
+  let page = loading ? (
+    <div
+      style={{ height: "100%", width: "100%" }}
+      className="flex-center-center"
+    >
+      <Loader />
+    </div>
+  ) : error ? (
+    `Error! ${error.message}`
+  ) : (
+    <div style={{ width: "100%" }}>test</div>
+  );
 
   return (
     <main>
       <ButtonNav back="customers" />
+      {page}
     </main>
   );
 };
