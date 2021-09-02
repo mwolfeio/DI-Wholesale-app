d481caee7c0b8c297de43532bc91e68a417065e9
mwoof <matthew.wolfe5@gmail.com>
2021-09-01 20:31:19 -0500

    added input style

--- pages/customers/index.js
+++ pages/customers/index.js
@@ -64,7 +64,11 @@ export default function SpecialPage({}) {
           Search, sort and select a store customer from the list below to edit
           things like customer number, metafields and membership points.
         </p>
-        <input placeholder="Enter a customer's name..."></input>
+        <input
+          className="list-search"
+          type="text"
+          placeholder="Enter a customer's name..."
+        ></input>
         <ul className="large-list customer-list">
           <li className="list-header">
             <p>Pic</p>
