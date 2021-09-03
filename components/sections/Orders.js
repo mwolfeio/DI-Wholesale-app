e203e186f0679efaa7bbe74ca766a74bc8ddd193
mwoof <90003090+mwolfeio@users.noreply.github.com>
2021-09-03 11:12:14 -0500

    forgot to add new component

---
+++ components/sections/Orders.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { useQuery } from "react-apollo";
+import { gql } from "apollo-boost";
+
+const Section = (props) => {
+  return (
+    <section>
+      <h2>Orders</h2>
+    </section>
+  );
+};
+export default Section;
