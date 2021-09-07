8de6c21a21e5753f74d4dbef8df58b9f369ea3a4
mwoof <90003090+mwolfeio@users.noreply.github.com>
2021-09-07 12:03:42 -0500

    fixed funciton name

--- components/MinimizeButton.js
+++ components/MinimizeButton.js
@@ -3,11 +3,16 @@ import Minimize from "../media/icons/Minimize.js";
 
 const Section = (props) => {
   return (
-    <div className="flex-center-btw" onClick={props.minimize}>
+    <div
+      className="flex-center-btw"
+      onClick={props.func}
+      style={{
+        marginLeft: "8px",
+      }}
+    >
       <button className="icon">
         <span
           style={{
-            marginLeft: "8px",
             transform: `rotate(${props.status ? 180 : 0}deg)`,
           }}
         >
