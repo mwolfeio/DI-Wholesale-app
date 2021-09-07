5a8daf94400a81c601c6ac72f6978cac6ebc67bf
mwoof <90003090+mwolfeio@users.noreply.github.com>
2021-09-07 12:04:58 -0500

    added no order message

--- components/sections/Orders.js
+++ components/sections/Orders.js
@@ -13,6 +13,8 @@ const Section = (props) => {
     setOpen(!open);
   };
 
+  console.log("ordersArr: ", ordersArr);
+
   return (
     <section>
       <SectionHeader
@@ -22,30 +24,34 @@ const Section = (props) => {
       />
       <div className="card-container">
         {open ? (
-          ordersArr.map((order, i) => (
-            <div className="card flex-center-btw">
-              <div>
-                <p>{order.node.totalPrice}</p>
-                <p>{order.node.totalPrice}</p>
-                <p>{order.node.createdAt}</p>
+          orders.length < 1 ? (
+            <div className="flex-center-center">No Orders</div>
+          ) : (
+            ordersArr.map((order, i) => (
+              <div className="card flex-center-btw">
+                <div>
+                  <p>{order.node.totalPrice}</p>
+                  <p>{order.node.totalPrice}</p>
+                  <p>{order.node.createdAt}</p>
+                </div>
+                <div className="virticle-divider"></div>
+                <div style={{ width: "100%" }} className="flex-center-left">
+                  {order.lineItems.edgest.map((product) => {
+                    <div className="order-product-wrapper">
+                      <span className="order-quant-badge">
+                        {product.node.quantity}
+                      </span>
+                      <img src={product.node.imgage.originalSrc} />
+                      <p>{product.node.title}</p>
+                      <p>{product.node.price}</p>
+                    </div>;
+                  })}
+                </div>
               </div>
-              <div className="virticle-divider"></div>
-              <div style={{ width: "100%" }} className="flex-center-left">
-                {order.lineItems.edgest.map((product) => {
-                  <div className="order-product-wrapper">
-                    <span className="order-quant-badge">
-                      {product.node.quantity}
-                    </span>
-                    <img src={product.node.imgage.originalSrc} />
-                    <p>{product.node.title}</p>
-                    <p>{product.node.price}</p>
-                  </div>;
-                })}
-              </div>
-            </div>
-          ))
+            ))
+          )
         ) : (
-          <div className="flex-center-center">No Orders</div>
+          ""
         )}
       </div>
     </section>
