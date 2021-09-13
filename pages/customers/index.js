bc68a6fa247bbd6cd48cc279abf4dda06b8f81fe
Matt <34251520+mwoof@users.noreply.github.com>
2021-09-13 09:49:29 -0400

    imported useRef

--- pages/customers/index.js
+++ pages/customers/index.js
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo, useEffect, useRef } from "react";
 import { useQuery } from "react-apollo";
 import { gql } from "apollo-boost";
 import _ from "lodash";
